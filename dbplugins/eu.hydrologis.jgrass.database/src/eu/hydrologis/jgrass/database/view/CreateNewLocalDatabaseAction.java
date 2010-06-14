e44a81f2ed19b5306060d9568b6ac7da1a6e0839
Andrea Antonello O <andrea.antonello@gmail.com>
2010-06-14 15:30:41 +0200

    adding view and actions structure - to be filled

---
+++ dbplugins/eu.hydrologis.jgrass.database/src/eu/hydrologis/jgrass/database/view/CreateNewLocalDatabaseAction.java
@@ -0,0 +1,28 @@
+package eu.hydrologis.jgrass.database.view;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+
+public class CreateNewLocalDatabaseAction implements IViewActionDelegate {
+
+    @Override
+    public void init( IViewPart view ) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void run( IAction action ) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void selectionChanged( IAction action, ISelection selection ) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
