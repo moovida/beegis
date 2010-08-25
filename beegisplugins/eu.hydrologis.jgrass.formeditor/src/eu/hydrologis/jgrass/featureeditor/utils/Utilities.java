430b3f1aed7d024a9a53cd1addac832edcefb539
Andrea Antonello O <andrea.antonello@gmail.com>
2010-08-25 13:21:59 +0200

    listeners are properly aligned

--- beegisplugins/eu.hydrologis.jgrass.formeditor/src/eu/hydrologis/jgrass/featureeditor/utils/Utilities.java
+++ beegisplugins/eu.hydrologis.jgrass.formeditor/src/eu/hydrologis/jgrass/featureeditor/utils/Utilities.java
@@ -18,8 +18,16 @@
 package eu.hydrologis.jgrass.featureeditor.utils;
 
 import java.io.File;
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 import org.apache.commons.io.FilenameUtils;
+
+import eu.hydrologis.jgrass.featureeditor.xml.annotated.Form;
 
 /**
  * Utilities for forms.
@@ -46,5 +54,33 @@ public class Utilities {
         } else {
             return null;
         }
+    }
+
+    /**
+     * Parse a form xml.
+     * 
+     * @param xml the xml containing the form definition. 
+     * @return the {@link Form}.
+     * @throws Exception
+     */
+    public static Form parseXML( String xml ) throws Exception {
+        JAXBContext jc = JAXBContext.newInstance("eu.hydrologis.jgrass.featureeditor.xml.annotated"); //$NON-NLS-1$
+        Unmarshaller um = jc.createUnmarshaller();
+        StringReader sr = new StringReader(xml);
+        return (Form) um.unmarshal(sr);
+    }
+
+    /**
+     * Write the {@link Form} to xml file.
+     * 
+     * @param form the {@link Form} object.
+     * @param file the file to which to dump to. 
+     * @throws Exception
+     */
+    public static void writeXML( Form form, File file ) throws Exception {
+        JAXBContext jc = JAXBContext.newInstance(Form.class);
+        Marshaller marshaller = jc.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(form, file);
     }
 }
