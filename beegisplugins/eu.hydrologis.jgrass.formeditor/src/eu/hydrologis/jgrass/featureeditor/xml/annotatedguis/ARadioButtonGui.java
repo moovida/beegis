11d9ae04b526c81050784030dcf9c160739a7a39
Andrea Antonello O <andrea.antonello@gmail.com>
2010-08-26 17:11:07 +0200

    combo and radio working

--- beegisplugins/eu.hydrologis.jgrass.formeditor/src/eu/hydrologis/jgrass/featureeditor/xml/annotatedguis/ARadioButtonGui.java
+++ beegisplugins/eu.hydrologis.jgrass.formeditor/src/eu/hydrologis/jgrass/featureeditor/xml/annotatedguis/ARadioButtonGui.java
@@ -18,7 +18,12 @@
  */
 package eu.hydrologis.jgrass.featureeditor.xml.annotatedguis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
@@ -34,34 +39,53 @@ import eu.hydrologis.jgrass.featureeditor.xml.annotated.FormElement;
  * 
  * @author Andrea Antonello (www.hydrologis.com)
  */
-public class ARadioButtonGui extends FormGuiElement {
-    private final ARadioButton radioButton;
+public class ARadioButtonGui extends FormGuiElement implements SelectionListener {
+    private final ARadioButton aRadioButton;
     private SimpleFeature feature;
+    private List<String> labelList;
+    private List<String> valuesList;
+    private List<Button> buttonsList;
 
     public ARadioButtonGui( ARadioButton radioButton ) {
-        this.radioButton = radioButton;
-
+        this.aRadioButton = radioButton;
     }
 
     @Override
     public Control makeGui( Composite parent ) {
 
         Composite radioComposite = new Composite(parent, SWT.NONE);
-        radioComposite.setLayoutData(radioButton.constraints);
+        radioComposite.setLayoutData(aRadioButton.constraints);
 
-        if (radioButton.orientation == null || radioButton.orientation.toLowerCase().startsWith("ver")) {
+        if (aRadioButton.orientation == null || aRadioButton.orientation.toLowerCase().startsWith("ver")) {
             radioComposite.setLayout(new GridLayout(1, false));
         } else {
-            radioComposite.setLayout(new GridLayout(radioButton.item.size(), false));
+            radioComposite.setLayout(new GridLayout(aRadioButton.item.size(), false));
+        }
+        labelList = new ArrayList<String>();
+        valuesList = new ArrayList<String>();
+        buttonsList = new ArrayList<Button>();
+
+        List<String> itemsList = aRadioButton.item;
+        for( String item : itemsList ) {
+            if (item.indexOf(',') != -1) {
+                String[] split = item.split(","); //$NON-NLS-1$
+                labelList.add(split[0].trim());
+                valuesList.add(split[1].trim());
+            } else {
+                labelList.add(item);
+                valuesList.add(item);
+            }
         }
 
-        for( String radioText : radioButton.item ) {
+        for( String radioText : labelList ) {
             Button button = new Button(radioComposite, SWT.RADIO);
             button.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
             button.setText(radioText);
-            if (radioButton.defaultText != null && radioButton.defaultText.equals(radioText)) {
+            if (aRadioButton.defaultText != null && aRadioButton.defaultText.equals(radioText)) {
                 button.setSelection(true);
             }
+            button.addSelectionListener(this);
+            buttonsList.add(button);
         }
 
         return radioComposite;
@@ -69,9 +93,50 @@ public class ARadioButtonGui extends FormGuiElement {
 
     public void setFeature( SimpleFeature feature ) {
         this.feature = feature;
+        updateRadio();
     }
-    
+
+    private void updateRadio() {
+        if (feature == null) {
+            return;
+        }
+        Object attribute = feature.getAttribute(aRadioButton.fieldName);
+        String attributeString = attribute.toString();
+
+        // find the button index and select it
+        int index = valuesList.indexOf(attributeString);
+        if (index == -1) {
+            index = 0;
+        }
+
+        for( int i = 0; i < buttonsList.size(); i++ ) {
+            Button button = buttonsList.get(i);
+            if (i == index) {
+                button.setSelection(true);
+            } else {
+                button.setSelection(false);
+            }
+        }
+
+    }
+
     public FormElement getFormElement() {
-        return radioButton;
+        return aRadioButton;
+    }
+
+    public void widgetSelected( SelectionEvent e ) {
+        Button source = (Button) e.getSource();
+
+        int index = buttonsList.indexOf(source);
+        String value = valuesList.get(index);
+        Class< ? > binding = feature.getProperty(aRadioButton.fieldName).getType().getBinding();
+
+        Object adapted = adapt(value, binding);
+        if (adapted != null) {
+            feature.setAttribute(aRadioButton.fieldName, adapted);
+        }
+    }
+
+    public void widgetDefaultSelected( SelectionEvent e ) {
     }
 }
