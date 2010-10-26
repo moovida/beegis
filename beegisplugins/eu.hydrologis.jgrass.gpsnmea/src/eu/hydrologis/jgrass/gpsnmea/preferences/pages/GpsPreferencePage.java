113d0410693c7e3fa0beec31b0474385c7bb2926
Andrea Antonello O <andrea.antonello@gmail.com>
2010-10-26 11:34:46 +0200

    removing gps settings page from preferences

--- beegisplugins/eu.hydrologis.jgrass.gpsnmea/src/eu/hydrologis/jgrass/gpsnmea/preferences/pages/GpsPreferencePage.java
+++ beegisplugins/eu.hydrologis.jgrass.gpsnmea/src/eu/hydrologis/jgrass/gpsnmea/preferences/pages/GpsPreferencePage.java
@@ -18,22 +18,9 @@
  */
 package eu.hydrologis.jgrass.gpsnmea.preferences.pages;
 
-import java.io.IOException;
-
-import org.eclipse.jface.preference.BooleanFieldEditor;
-import org.eclipse.jface.preference.FieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
-import org.eclipse.jface.preference.IntegerFieldEditor;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.eclipse.ui.preferences.ScopedPreferenceStore;
-
-import eu.hydrologis.jgrass.gpsnmea.GpsActivator;
 
 /**
  * The Gps preference page.
@@ -44,35 +31,17 @@ public class GpsPreferencePage extends FieldEditorPreferencePage
         implements
             IWorkbenchPreferencePage {
 
-    private ScopedPreferenceStore preferences = null;
 
     public GpsPreferencePage() {
         super(GRID);
-        preferences = (ScopedPreferenceStore) GpsActivator.getDefault().getPreferenceStore();
-        setPreferenceStore(preferences);
-        setDescription("GPS settings page");
+        setDescription("Various GPS settings");
     }
 
     public boolean performOk() {
-        try {
-            preferences.save();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return super.performOk();
     }
 
-    public void createFieldEditors() {
-
-        addField(new SerialPortsFieldEditor(PreferenceConstants.PORTUSED, getFieldEditorParent()));
-        addField(new StartStopGpsFieldEditor(PreferenceConstants.GPS_IS_ON, getFieldEditorParent()));
-        addField(new IntegerFieldEditor(PreferenceConstants.INTERVAL_SECONDS,
-                PreferenceConstants.INTERVAL_SECONDS, getFieldEditorParent()));
-        addField(new DoubleFieldEditor(PreferenceConstants.DISTANCE_THRESHOLD,
-                PreferenceConstants.DISTANCE_THRESHOLD, getFieldEditorParent()));
-        addField(new BooleanFieldEditor(PreferenceConstants.TESTMODE,
-                "Use a dummy dataset for testing", 1, getFieldEditorParent()));
-    }
+    public void createFieldEditors() {}
 
     /*
      * (non-Javadoc)
@@ -81,54 +50,4 @@ public class GpsPreferencePage extends FieldEditorPreferencePage
      */
     public void init( IWorkbench workbench ) {
     }
-
-    static class DoubleFieldEditor extends FieldEditor {
-
-        Label label;
-        Text text;
-        public DoubleFieldEditor( String name, String labelText, Composite parent ) {
-            super(name, labelText, parent);
-        }
-
-        protected void adjustForNumColumns( int numColumns ) {
-
-        }
-
-        @Override
-        protected void doFillIntoGrid( Composite comp, int numColumns ) {
-            label = new Label(comp, SWT.NONE);
-            label.setText(super.getLabelText());
-            label.setLayoutData(new GridData(SWT.FILL, SWT.NONE, false, false));
-            text = new Text(comp, SWT.SINGLE | SWT.BORDER);
-            text.setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
-        }
-
-        @Override
-        protected void doLoad() {
-            text.setText(String.valueOf(getPreferenceStore().getDouble(super.getPreferenceName())));
-        }
-
-        @Override
-        protected void doLoadDefault() {
-            text.setText(String.valueOf(getPreferenceStore().getDefaultDouble(
-                    super.getPreferenceName())));
-        }
-
-        @Override
-        protected void doStore() {
-            try {
-                getPreferenceStore().setValue(super.getPreferenceName(),
-                        Double.valueOf(text.getText()));
-            } catch (Exception e) {
-                // you've been warned
-            }
-        }
-
-        @Override
-        public int getNumberOfControls() {
-            return 2;
-        }
-
-    }
-
 }
