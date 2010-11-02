6cf2e3b9b995dff52ae9d7f65e89dc82d122b846
Andrea Antonello H <andrea.antonello@gmail.com>
2010-11-02 09:03:48 +0100

    adding missing classes

---
+++ beegisplugins/eu.hydrologis.jgrass.formeditor/src/eu/hydrologis/jgrass/formeditor/utils/Constants.java
@@ -0,0 +1,8 @@
+package eu.hydrologis.jgrass.formeditor.utils;
+
+public class Constants {
+    public static final int PIXEL_SNAP = 20;
+    public static final int INSETS = 5;
+    public static final int FIELDNAME_OFFSET_X = 30;
+    public static final int FIELDNAME_OFFSET_Y = 5;
+}
