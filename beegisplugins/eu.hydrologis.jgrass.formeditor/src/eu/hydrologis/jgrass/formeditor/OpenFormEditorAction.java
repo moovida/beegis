2eb42e52ce1b56b53ebbbdec0bc1b315bd464101
Andrea Antonello O <andrea.antonello@gmail.com>
2011-03-18 16:02:43 +0100

    support for non file based layers - formeditor part

--- beegisplugins/eu.hydrologis.jgrass.formeditor/src/eu/hydrologis/jgrass/formeditor/OpenFormEditorAction.java
+++ beegisplugins/eu.hydrologis.jgrass.formeditor/src/eu/hydrologis/jgrass/formeditor/OpenFormEditorAction.java
@@ -23,6 +23,7 @@ import net.refractions.udig.catalog.ID;
 import net.refractions.udig.catalog.IGeoResource;
 import net.refractions.udig.project.ILayer;
 import net.refractions.udig.project.IMap;
+import net.refractions.udig.project.IStyleBlackboard;
 import net.refractions.udig.project.ui.ApplicationGIS;
 
 import org.apache.commons.io.FilenameUtils;
@@ -31,6 +32,7 @@ import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
@@ -38,6 +40,7 @@ import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.IWorkbenchWindowActionDelegate;
+import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.geotools.data.FeatureSource;
@@ -66,65 +69,81 @@ public class OpenFormEditorAction implements IWorkbenchWindowActionDelegate {
                 try {
 
                     IMap activeMap = ApplicationGIS.getActiveMap();
+                    ILayer selectedLayer = null;
                     if (activeMap != null) {
-                        ILayer selectedLayer = activeMap.getEditManager().getSelectedLayer();
-                        if (selectedLayer.hasResource(FeatureSource.class)) {
-                            IGeoResource geoResource = selectedLayer.getGeoResource();
-                            ID id = geoResource.getID();
-                            boolean isFile = id.isFile();
-                            if (isFile) {
-                                File f = id.toFile();
-                                String baseName = FilenameUtils.getBaseName(f.getName());
-                                File newF = new File(f.getParentFile(), baseName + ".form");
-                                if (!newF.exists()) {
-                                    newF.createNewFile();
-                                }
-
-                                final IPath ipath = new Path(newF.getAbsolutePath());
-                                IFileStore fileLocation = EFS.getLocalFileSystem().getStore(ipath);
-                                FileStoreEditorInput fileStoreEditorInput = new FileStoreEditorInput(fileLocation);
-                                IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-                                page.openEditor(fileStoreEditorInput, FormEditor.ID);
-
-                                PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-                                        .showView("org.eclipse.ui.views.PropertySheet");
-
-                                return;
-                            }
-
-                        }
-
+                        selectedLayer = activeMap.getEditManager().getSelectedLayer();
+                    } else {
+                        MessageDialog.openError(window.getShell(), "ERROR",
+                                "To use the form editor you need to select a feature layer to work on.");
+                        return;
+                    }
+                    if (selectedLayer == null || !selectedLayer.hasResource(FeatureSource.class)) {
+                        MessageDialog.openError(window.getShell(), "ERROR",
+                                "To use the form editor you need to select a feature layer to work on.");
+                        return;
                     }
 
-                    FileDialog fileDialog = new FileDialog(window.getShell(), SWT.OPEN);
-                    fileDialog.setFilterExtensions(new String[]{"*.form"});
-                    String path = fileDialog.open();
+                    IGeoResource geoResource = selectedLayer.getGeoResource();
+                    ID id = geoResource.getID();
+                    boolean isFile = id.isFile();
+                    if (isFile) {
+                        File f = id.toFile();
+                        String baseName = FilenameUtils.getBaseName(f.getName());
+                        File newF = new File(f.getParentFile(), baseName + ".form");
+                        if (!newF.exists()) {
+                            newF.createNewFile();
+                        }
 
+                        openEditor(newF);
+
+                        return;
+                    }
+                    /*
+                     * try to see if we can find one in the bboard for this layer
+                     */
+                    IStyleBlackboard blackboard = selectedLayer.getStyleBlackboard();
+                    String path = blackboard.getString(FormEditor.ID);
+                    if (path != null) {
+                        final File f = new File(path);
+                        openEditor(f);
+                        return;
+                    }
+                    /*
+                     * it is not file based or no project/map is there. Just ask 
+                     * for a file on which to save.
+                     */
+                    FileDialog fileDialog = new FileDialog(window.getShell(), SWT.SAVE);
+                    fileDialog.setText("Please select the file to which to save the form.");
+                    fileDialog.setFilterExtensions(new String[]{"*.form"});
+                    path = fileDialog.open();
                     if (path == null || path.length() < 1) {
                         return;
                     }
 
                     final File f = new File(path);
-                    if (!f.exists()) {
-                        return;
-                    }
-
-                    final IPath ipath = new Path(f.getAbsolutePath());
-                    IFileStore fileLocation = EFS.getLocalFileSystem().getStore(ipath);
-                    FileStoreEditorInput fileStoreEditorInput = new FileStoreEditorInput(fileLocation);
-                    IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-                    page.openEditor(fileStoreEditorInput, FormEditor.ID);
-
-                    PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage()
-                            .showView("org.eclipse.ui.views.PropertySheet");
+                    openEditor(f);
+                    // the path will live in the bboard, if not as a sidecar file
+                    blackboard.putString(FormEditor.ID, path);
 
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+
         });
 
     }
+
+    private void openEditor( File newF ) throws PartInitException {
+        final IPath ipath = new Path(newF.getAbsolutePath());
+        IFileStore fileLocation = EFS.getLocalFileSystem().getStore(ipath);
+        FileStoreEditorInput fileStoreEditorInput = new FileStoreEditorInput(fileLocation);
+        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        page.openEditor(fileStoreEditorInput, FormEditor.ID);
+
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView("org.eclipse.ui.views.PropertySheet");
+    }
+
     public void selectionChanged( IAction action, ISelection selection ) {
         // TODO Auto-generated method stub
 
